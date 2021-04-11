6346eb992d54a3818821144b18d651bd02174473
Erwin Alan Frías Martínez <alan.frias@erwcode.com>
2021-04-11 15:35:14 -0500

    ecosystem component :sushi:

--- src/index.js
+++ src/index.js
@@ -11,11 +11,15 @@ import logotipo from './images/selectoral-logo.svg';
 
 import screenDashboardS from './images/selectoral-dashboard@1x.png'
 import screenDashboardM from './images/selectoral-dashboard.png'
+import screenLogin from './images/selectoral-login.png'
 
 import iconArrow from './images/icon-arrow.svg'
 import iconCaptura from './images/icon-captura.svg'
 import iconAnaliza from './images/icon-analiza.svg'
 import iconPlanifica from './images/icon-planifica.svg'
+import iconPerfiles from './images/icon-perfiles.svg'
+import iconEstructura from './images/icon-estructura.svg'
+import iconReportes from './images/icon-reportes.svg'
 
 import vectorTop from './images/estructura-top.png'
 
